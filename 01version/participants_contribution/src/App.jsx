f8795b7fc906b03e1a084c115a539225f728baf8
tirthankarg <tirthankar158@gmail.com>
2025-09-10 16:26:03 +0530

    first contribution

--- 01version/participants_contribution/src/App.jsx
+++ 01version/participants_contribution/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Shailee from './participants_card/Shailee'
 import Taufique from './participants_card/Taufique'
+import Tirthankar from './participants_card/Tirthankar'
 
 function App() {
   return (
     <div>
       <Shailee/>
       <Taufique/>
+      <Tirthankar/>
     </div>
   )
 }
