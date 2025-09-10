36fb7cd813ca77e6ed465bb87cdbf28afddf2a44
abhilipsa <abhilipsaabhi54@gmail.com>
2025-09-10 19:46:57 +0530

    Abhilipsa card added

--- 01version/participants_contribution/src/App.jsx
+++ 01version/participants_contribution/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Shailee from './participants_card/Shailee'
 import Taufique from './participants_card/Taufique'
+import Abhilipsa from './participants_card/Abhilipsa'
 
 function App() {
   return (
     <div>
       <Shailee/>
       <Taufique/>
+      <Abhilipsa/>
     </div>
   )
 }
