a23b0775fb2dc2b94bf425a4b856ab5086cd2d23
Satya-291 <mishrasatyaranjan291@gmail.com>
2025-09-11 19:48:16 +0530

    First contibution from Satyaranjan

--- 01version/participants_contribution/src/App.jsx
+++ 01version/participants_contribution/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Shailee from './participants_card/Shailee'
 import Taufique from './participants_card/Taufique'
+import SatyaM from './participants_card/SatyaM'
 
 function App() {
   return (
     <div>
       <Shailee/>
       <Taufique/>
+       <SatyaM/> 
     </div>
   )
 }
