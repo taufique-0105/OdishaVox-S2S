924b97fbcc13812a335a2e5689ed9c93d7f64909
Aswin889 <biswalaswinikumar9@gmail.com>
2025-09-11 20:46:35 +0530

    1st commit from Ashwin

--- 01version/participants_contribution/src/App.jsx
+++ 01version/participants_contribution/src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Shailee from './participants_card/Shailee'
 import Taufique from './participants_card/Taufique'
-
+import Ashwin from './participants_card/Ashwin'
 function App() {
   return (
     <div>
       <Shailee/>
       <Taufique/>
+      <Ashwin/>
     </div>
   )
 }
---
+++ 01version/participants_contribution/src/participants_card/Ashwin.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Ashwin = () => {
+  return (
+    <div className="bg-white p-4 text-black rounded-2xl m-5">
+      {/* <img src={imageUrl} alt={title} className="card-image" /> */}
+      <div className="">
+        <h3 className="text-xl font-bold">Ashwin</h3>
+        <p className="">I am Ashwin from Jagatsinghpur, Odisha.</p>
+        <button className=" text-white bg-red-600">Learn More</button>
+      </div>
+    </div>
+  )
+}
+
+export default Ashwin ;
+
