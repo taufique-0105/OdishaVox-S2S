82aaa17b51e91fe15319f9cab4118bece10b542d
mohit1988 <microprosoftware7@gmail.com>
2025-09-11 20:20:50 +0530

    first commit with user mohit

--- 01version/participants_contribution/src/App.jsx
+++ 01version/participants_contribution/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Shailee from './participants_card/Shailee'
 import Taufique from './participants_card/Taufique'
+import Mohit from './participants_card/Mohit'
 
 function App() {
   return (
     <div>
       <Shailee/>
       <Taufique/>
+      <Mohit/>
     </div>
   )
 }
