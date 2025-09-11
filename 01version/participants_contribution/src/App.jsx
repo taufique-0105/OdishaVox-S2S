a53c08a7080ecd485f1285bf59ab69067fa4a6b0
Nikhhil333 <nikhhil.ace7@gmail.com>
2025-09-11 20:02:06 +0530

    First contribution from Nikhhil

--- 01version/participants_contribution/src/App.jsx
+++ 01version/participants_contribution/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Shailee from './participants_card/Shailee'
 import Taufique from './participants_card/Taufique'
+import Nikhhil from './participants_card/Nikhhil'
 
 function App() {
   return (
     <div>
       <Shailee/>
       <Taufique/>
+      <Nikhhil/> 
     </div>
   )
 }
